6df92a1664cea024ffd495238a3bd980be5637e0
Brandon Shearin <brandon.shearin@trove.co>
2022-09-04 11:35:21 -0700

    elements are randomly placed on page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import React from 'react'
 import shapes from './shapes'
 
 function App() {
-  // const [animated, setAnimated] = React.useState(false)
+  const [y, setY] = React.useState(0)
+  const [x, setX] = React.useState(0)
 
-  return shapes.map(({component, name}, index) => {
-    return React.createElement(component, {className: `shape-${index} ${name}`})
-  })
+  React.useEffect(() => {
+    const {innerHeight, innerWidth} = window
+    setY(innerHeight)
+    setX(innerWidth)
+  }, [])
+
+  return (
+    <div className='root'>
+      {
+        shapes.map(({component, name}, index) => {
+          return React.createElement(component, {
+            className: `shape-${index} ${name}` ,
+            style: {
+              position: 'absolute',
+              left: Math.random() * x,
+              top: Math.random() * y
+            }})
+        })
+      }
+    </div>
+  )
 }
-
-
-
 
 export default App;
